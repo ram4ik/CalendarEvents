69dd20b691c88ce47a9b0c9c71121147412fb847
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-09-07 16:55:09 +0300

    Initial Commit

---
+++ CalendarEvents/CalendarEventsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarEventsApp.swift
+//  CalendarEvents
+//
+//  Created by ramil on 07.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct CalendarEventsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
