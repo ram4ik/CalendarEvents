58f91b6ace3aacfcb236af0a7a835c3c5c697b02
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-09-07 17:13:10 +0300

    init

--- CalendarEvents/ContentView.swift
+++ CalendarEvents/ContentView.swift
@@ -6,11 +6,69 @@
 //
 
 import SwiftUI
+import EventKit
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button(action: {
+            checkPermission(startDate: Date(), eventName: "Meeting")
+        }, label: {
+            Text("Set up event")
+        })
+    }
+    
+    func checkPermission(startDate: Date, eventName: String) {
+        
+        let eventStore = EKEventStore()
+        
+        switch EKEventStore.authorizationStatus(for: .event) {
+        
+        
+        case .notDetermined:
+            eventStore.requestAccess(to: .event) { (status, error) in
+                if status {
+                    insertEvent(store: eventStore, startDate: startDate, eventName: eventName)
+                } else {
+                    print(error?.localizedDescription)
+                }
+            }
+        case .restricted:
+            print("restricted")
+        case .denied:
+            print("denied")
+        case .authorized:
+            insertEvent(store: eventStore, startDate: startDate, eventName: eventName)
+        @unknown default:
+            print("Unknown")
+        }
+    }
+    
+    func insertEvent(store: EKEventStore, startDate: Date, eventName: String) {
+        
+        let calendars = store.calendars(for: .event)
+        
+        for calendar in calendars {
+            if calendar.title == "Calendar" {
+                let event = EKEvent(eventStore: store)
+                event.calendar = calendar
+                event.startDate = startDate
+                event.title = eventName
+                event.endDate = event.startDate.addingTimeInterval(60)
+                event.recurrenceRules = [EKRecurrenceRule(recurrenceWith: .daily, interval: 1, end: nil)]
+                
+                let reminder1 = EKAlarm(relativeOffset: -60)
+                let reminder2 = EKAlarm(relativeOffset: -300)
+                
+                event.alarms = [reminder1, reminder2]
+                
+                do {
+                    try store.save(event, span: .thisEvent)
+                    print("event insert")
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+        }
     }
 }
 
@@ -19,3 +77,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
